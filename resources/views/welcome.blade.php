b5c11da70e9898cc4ebe9bc024c153c75e2b1624
meh <meh>
2018-06-07 06:13:37 +0000

    added user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to Microposts</h1>
+            {!! link_to_route('signup.get', 'Signup', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
